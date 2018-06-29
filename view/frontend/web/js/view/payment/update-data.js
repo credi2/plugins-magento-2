2213a491b921dacce16786cd8ab869fe36c72cf6
Anton Sannikov <a.sannikov@limesoda.com>
2018-06-29 18:08:57 +0200

    fix billing address

---
+++ view/frontend/web/js/view/payment/update-data.js
@@ -0,0 +1,93 @@
+define([
+    'jquery',
+    'Magento_Checkout/js/model/quote'
+], function ($, quote){
+    'use strict';
+
+    return function () {
+        var refreshData = {};
+
+        var email = quote.guestEmail ? quote.guestEmail : window.checkoutConfig.customerData.email;
+
+        if (email) {
+            refreshData.email = email;
+        }
+
+        var useShippingAddress = $('.payment-method._active input[name=billing-address-same-as-shipping]').is(':checked');
+
+        var firstname = useShippingAddress ? quote.shippingAddress().firstname : quote.billingAddress().firstname;
+
+        if (firstname) {
+            refreshData.given = firstname;
+        }
+
+        var lastname = useShippingAddress ? quote.shippingAddress().lastname : quote.billingAddress().lastname;
+
+        if (lastname) {
+            refreshData.family = lastname;
+        }
+
+        var telephone = useShippingAddress ? quote.shippingAddress().telephone : quote.billingAddress().telephone;
+
+        if (telephone) {
+            refreshData.phone = lastname;
+        }
+
+        var postcode = useShippingAddress ? quote.shippingAddress().postcode : quote.billingAddress().postcode;
+
+        if (postcode) {
+            refreshData.zip = postcode;
+        }
+
+        var city = useShippingAddress ? quote.shippingAddress().city : quote.billingAddress().city;
+
+        if (city) {
+            refreshData.city = city;
+        }
+
+        var countryId = useShippingAddress ? quote.shippingAddress().countryId : quote.billingAddress().countryId;
+
+        if (countryId) {
+            refreshData.country = countryId;
+        }
+
+        var street = useShippingAddress ? quote.shippingAddress().street : quote.billingAddress().street;
+
+        if (street instanceof Array) {
+            var address = street.join(', ');
+
+            if (address) {
+                refreshData.addressline = address;
+            }
+        }
+
+        /*var dob = window.checkoutConfig.customerData.dob;
+
+        if (dob) {
+            refreshData.birthdate = dob;
+        }*/
+
+        var vatId = useShippingAddress ? quote.shippingAddress().vatId : quote.billingAddress().vatId;
+
+        if (vatId) {
+            refreshData.iban = vatId;
+        }
+
+        var paymentMethod = quote.getPaymentMethod()();
+
+        if (paymentMethod && quote.getPaymentMethod && window.checkoutConfig.payment[paymentMethod.method].debug) {
+            console.log(refreshData);
+            console.log(quote.totals()['grand_total']);
+        }
+
+        if (window.C2EcomCheckout) {
+            window.C2EcomCheckout.refresh();
+            $('#c2CheckoutScript').attr('data-c2-amount', quote.totals()['grand_total']);
+            window.C2EcomCheckout.init();
+            window.C2EcomCheckout.refreshOptionalData(refreshData);
+            return true;
+        }
+
+        return false;
+    }
+});
